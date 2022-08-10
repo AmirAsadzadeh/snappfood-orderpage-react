9c805108e1279c3f75d3a36bad59e39c54209a80
Amir Asadzadeh <amiraliasd79@gmail.com>
2022-08-10 09:29:19 +0430

    some errors in console fixed

--- src/Components/Modal/Layer.js
+++ src/Components/Modal/Layer.js
@@ -1,7 +1,6 @@
 import classes from "./Layer.module.css";
 
 import React from "react";
-import ReactDOM from "react-dom";
 
 function Layer(props) {
   return <div className={classes.layer} onClick={props.onClick}></div>;
