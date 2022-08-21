2ffa635d173ab4f349109e0df4f6afcdd477b255
Amir Asadzadeh <amiraliasd79@gmail.com>
2022-08-21 17:26:56 +0430

    some changes

--- src/Components/MainContent/FoodsList/Food.js
+++ src/Components/MainContent/FoodsList/Food.js
@@ -5,12 +5,33 @@ import ReactDom from "react-dom";
 import RestaurantInfo from "../../../Context/restaurant-info";
 import CartInfo from "../../../Context/cart-info";
 import SpecialItemsModal from "../../Modal/SpecialItemsModal";
+import Layer from "../../Modal/Layer";
 
 function Food(props) {
   const { isOpen: restaurantIsOpen } = useContext(RestaurantInfo);
   const cartInfoCtx = useContext(CartInfo);
 
-  const [hasModal, setHasModal] = useState(true);
+  // const
+
+  const [hasModal, setHasModal] = useState(false);
+  const [needModal, setNeedModal] = useState(null);
+  const [orderData, setOrderData] = useState("");
+
+  const modalCloseHandler = function () {
+    setHasModal(false);
+  };
+
+  const buttonClickHandler = function (specials, component, data) {
+    if (Object.keys(specials).length > 0) {
+      setHasModal(true);
+      setNeedModal(component);
+      setOrderData(data);
+    } else {
+      cartInfoCtx.controller(data);
+    }
+  };
+
+  const addSpecialItemFormSubmitHandler = function () {};
 
   const foodInfo = props.food;
 
@@ -26,49 +47,72 @@ function Food(props) {
         </div>
       </div>
       <ul className={classes["type-list"]}>
-        {foodInfo.types.map((item) => (
-          <li key={item.id}>
-            <div className={classes["types-container"]}>
-              <div>
-                <p className={classes["type-title"]}>{item.title}</p>
-                <div className={classes["types-price-container"]}>
-                  <p className={classes["type-price"]}>
-                    {new Intl.NumberFormat("fa-IR").format(item.price)} تومان
-                  </p>
+        {foodInfo.types.map((item) => {
+          return (
+            <li key={item.id}>
+              <div className={classes["types-container"]}>
+                <div>
+                  <p className={classes["type-title"]}>{item.title}</p>
+                  <div className={classes["types-price-container"]}>
+                    <p className={classes["type-price"]}>
+                      {new Intl.NumberFormat("fa-IR").format(item.price)} تومان
+                    </p>
+                  </div>
                 </div>
+                <button
+                  className={classes["type-button"]}
+                  disabled={!restaurantIsOpen}
+                  onClick={buttonClickHandler.bind(
+                    null,
+                    item.specials,
+                    item.id,
+                    {
+                      actionType: "add",
+                      foodData: {
+                        name: foodInfo.name,
+                        id: foodInfo.foodId,
+                        originalPrice: item.price,
+                        discountedPrice:
+                          item.price - (item.price * foodInfo.discount) / 100,
+                        discount: foodInfo.discount,
+                        foodType: item.title,
+                        specialItems: [],
+                      },
+                    }
+                  )}
+                >
+                  افزودن
+                </button>
+
+                {hasModal &&
+                  needModal === item.id &&
+                  ReactDom.createPortal(
+                    <Layer onClick={modalCloseHandler} />,
+                    document.querySelector("#layer")
+                  )}
+
+                {hasModal &&
+                  needModal === item.id &&
+                  ReactDom.createPortal(
+                    <SpecialItemsModal
+                      foodName={foodInfo.name}
+                      type={item.title}
+                      specials={item.specials}
+                      onClose={modalCloseHandler}
+                      onSubmit={cartInfoCtx.controller}
+                      // data={}
+                    />,
+                    document.querySelector("#modal-message")
+                  )}
               </div>
-              <button
-                className={classes["type-button"]}
-                disabled={!restaurantIsOpen}
-                onClick={cartInfoCtx.controller.bind(null, {
-                  actionType: "add",
-                  foodData: {
-                    name: foodInfo.name,
-                    id: foodInfo.foodId,
-                    originalPrice: item.price,
-                    discountedPrice:
-                      item.price - (item.price * foodInfo.discount) / 100,
-                    discount: foodInfo.discount,
-                    foodType: item.title,
-                  },
-                })}
-              >
-                افزودن
-              </button>
-              {hasModal &&
-                ReactDom.createPortal(
-                  <SpecialItemsModal
-                    foodName={foodInfo.name}
-                    type={item.title}
-                  />,
-                  document.querySelector("#modal-message")
-                )}
-            </div>
-          </li>
-        ))}
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
 }
 
 export default Food;
+
+// cartInfoCtx.controller.bind(null);
