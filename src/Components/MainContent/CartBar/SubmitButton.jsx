9189925f120eabbded2b76279040bb37d690e844
Amir Asadzadeh <amiraliasd79@gmail.com>
2022-09-29 19:01:30 +0330

    submit button click handler implemented

--- src/Components/MainContent/CartBar/SubmitButton.jsx
+++ src/Components/MainContent/CartBar/SubmitButton.jsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import { useContext } from "react";
 import UserInfo from "../../../Context/user-info";
 import classes from "./SubmitButton.module.css";
+import LoginModal from "../../Modal/LoginModal";
+import Layer from "../../Modal/Layer";
 
 function SubmitButton() {
   const userInfoCtx = useContext(UserInfo);
+
+  const [hasModal, setHasModal] = useState(false);
+
+  const submitButtonClickHandler = function () {
+    setHasModal(true);
+  };
+
+  const modalCloseHandler = function () {
+    setHasModal(false);
+  };
   return (
     <div className={classes["button-container"]}>
-      <div className={classes.button} role="button">
-        {userInfoCtx.isLoggedIn ? "پرداخت" : "ورود به حساب کاربری"}
-      </div>
+      {userInfoCtx.isLoggedIn ? (
+        <div
+          className={classes.button}
+          role="button"
+          onClick={submitButtonClickHandler}
+        >
+          پرداخت
+        </div>
+      ) : (
+        <div
+          className={classes.button}
+          role="button"
+          onClick={submitButtonClickHandler}
+        >
+          ورود به حساب کاربری
+        </div>
+      )}
+      {hasModal &&
+        ReactDOM.createPortal(
+          <LoginModal onClose={modalCloseHandler} />,
+          document.getElementById("modal-message")
+        )}
+      {hasModal &&
+        ReactDOM.createPortal(
+          <Layer onClick={modalCloseHandler} />,
+          document.getElementById("layer")
+        )}
     </div>
   );
 }
