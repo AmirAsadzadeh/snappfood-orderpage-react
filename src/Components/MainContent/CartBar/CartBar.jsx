766ba8b7ab0951a89398549e13ab207dd632732a
Amir Asadzadeh <amiraliasd79@gmail.com>
2022-09-29 09:59:00 +0330

    empty cart component implemented

--- src/Components/MainContent/CartBar/CartBar.jsx
+++ src/Components/MainContent/CartBar/CartBar.jsx
@@ -4,13 +4,16 @@ import React from "react";
 import SendTime from "./SendTime";
 import SendingService from "./SendingService";
 import CartContainer from "./CartContainer";
+import { useSelector } from "react-redux";
+import EmptyCart from "./EmptyCart";
 
 function CartBar() {
+  const cartCount = useSelector((state) => state.totalCounts);
   return (
     <section className={classes["cart-bar"]}>
       <SendTime />
       <SendingService />
-      <CartContainer />
+      {cartCount > 0 ? <CartContainer /> : <EmptyCart />}
     </section>
   );
 }
