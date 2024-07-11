348db999743273ac5bbc809481257dc5de8f457c
Amir <93188142+AmirAsadzadeh@users.noreply.github.com>
2024-07-11 10:50:01 +0330

    Update ControlButtons.jsx

--- src/Components/UI/ControlButtons.jsx
+++ src/Components/UI/ControlButtons.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./ControlButtons.module.css";
-import { formatNumber } from "../Helper/numberFormatter";
+import { formatNumber } from "../Helper/NumberFormatter";
 
 const garbageIcon = (
   <svg
